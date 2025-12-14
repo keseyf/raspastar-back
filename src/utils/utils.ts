0362f1411e55d8e51334e7e4a8f17667d7697d5f
fye <keseyf.exe@proton.me>
2025-12-14 19:57:59 -0300

    Adjust vercel file (again 2x)

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,2 @@
-import fastify from "fastify";
 import jsonwebtoken from "jsonwebtoken"
-import { PrismaClient } from "@prisma/client";
-
-export const prisma = new PrismaClient()
 export const jwt = jsonwebtoken
