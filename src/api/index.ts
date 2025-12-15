b92b18c41b3223ad0ec408f451f8650ae085baab
fye <keseyf.exe@proton.me>
2025-12-14 23:45:00 -0300

    Adjust vercel file (again 16x)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
-import { app } from "../app"
+// api/index.ts
+import { app } from "../app"; // app deve ser compilado para dist via tsc
 
 export default async function handler(req: any, res: any) {
-  await app.ready()
-  app.server.emit("request", req, res)
+  await app.ready();
+  app.server.emit("request", req, res);
 }
