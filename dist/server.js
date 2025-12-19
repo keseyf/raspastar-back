f49094c416f3d02dd4645713430692de1c576298
fye <keseyf.exe@proton.me>
2025-12-19 00:18:28 -0300

    alteracao nos premios e chances de ganhos

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,7 @@
 "use strict";
-// import { app } from "./app"
-// const port = Number(process.env.PORT || 4444)
-// app.listen({ port }, () => {
-//   console.log(`🔥 API rodando em http://localhost:${port}`)
-// })
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = require("./app");
+const port = Number(process.env.PORT || 4444);
+app_1.app.listen({ port }, () => {
+    console.log(`🔥 API rodando em http://localhost:${port}`);
+});
--- src/utils/prizes.ts
+++ src/utils/prizes.ts
@@ -1,8 +1,8 @@
 export const game1Prizes = [
-    {id: "iPhone 15 Pro Max", winChance: 0.02, imgUrl: "iphone15icon.png", isCashPrize: false},  // Produto físico
-    {id: "Moto Honda CG 160", winChance: 0.05, imgUrl: "moto_icon.png", isCashPrize: false},     // Produto físico
-    {id: "Scooter elétrica", winChance: 0.08, imgUrl: "scooter_icon.png", isCashPrize: false},     // Produto físico
-    {id: "Notebook Gamer", winChance: 0.07, imgUrl: "notebook_icon.png", isCashPrize: false},     // Produto físico
+    {id: "iPhone 15 Pro Max", winChance: 0.01, imgUrl: "iphone15icon.png", isCashPrize: false},  // Produto físico
+    {id: "Moto Honda CG 160", winChance: 0.03, imgUrl: "moto_icon.png", isCashPrize: false},     // Produto físico
+    {id: "Scooter elétrica", winChance: 0.04, imgUrl: "scooter_icon.png", isCashPrize: false},     // Produto físico
+    {id: "Notebook Gamer", winChance: 0.05, imgUrl: "notebook_icon.png", isCashPrize: false},     // Produto físico
     {id: "Smartwatch Premium", winChance: 0.1, imgUrl: "smartwatch_icon.png", isCashPrize: false}, // Produto físico
     {id: "PIX R$1000", winChance: 0.1, imgUrl: "pix1000_icon.png", isCashPrize: true},            // Dinheiro
     {id: "PIX R$100", winChance: 0.12, imgUrl: "pix100_icon.png", isCashPrize: true},             // Dinheiro
@@ -14,12 +14,12 @@ export const game1Prizes = [
 ];
 
 export const game2Prizes = [
-    {id: "iPhone 14 Pro", winChance: 0.03, imgUrl: "iphone14pro_icon.png", isCashPrize: false},   // Produto físico
-    {id: "Smart TV 65 polegadas", winChance: 0.05, imgUrl: "tv65_icon.png", isCashPrize: false},  // Produto físico
-    {id: "Máquina de lavar moderna", winChance: 0.07, imgUrl: "maquina_lavar_icon.png", isCashPrize: false}, // Produto físico
-    {id: "Tablet Samsung Galaxy", winChance: 0.08, imgUrl: "tablet_samsung_icon.png", isCashPrize: false}, // Produto físico
-    {id: "Fone Bluetooth Top de Linha", winChance: 0.1, imgUrl: "fone_bluetooth_icon.png", isCashPrize: false}, // Produto físico
-    {id: "PIX R$1000", winChance: 0.1, imgUrl: "pix1000_icon.png", isCashPrize: true},           // Dinheiro
+    {id: "iPhone 14 Pro", winChance: 0.01, imgUrl: "iphone14pro_icon.png", isCashPrize: false},   // Produto físico
+    {id: "Smart TV 65 polegadas", winChance: 0.02, imgUrl: "tv65_icon.png", isCashPrize: false},  // Produto físico
+    {id: "Máquina de lavar moderna", winChance: 0.03, imgUrl: "maquina_lavar_icon.png", isCashPrize: false}, // Produto físico
+    {id: "Tablet Samsung Galaxy", winChance: 0.04, imgUrl: "tablet_samsung_icon.png", isCashPrize: false}, // Produto físico
+    {id: "Fone Bluetooth Top de Linha", winChance: 0.02, imgUrl: "fone_bluetooth_icon.png", isCashPrize: false}, // Produto físico
+    {id: "PIX R$1000", winChance: 0.01, imgUrl: "pix1000_icon.png", isCashPrize: true},           // Dinheiro
     {id: "PIX R$100", winChance: 0.12, imgUrl: "pix100_icon.png", isCashPrize: true},            // Dinheiro
     {id: "PIX R$50", winChance: 0.12, imgUrl: "pix50_icon.png", isCashPrize: true},              // Dinheiro
     {id: "PIX R$10", winChance: 0.12, imgUrl: "pix10_icon.png", isCashPrize: true},              // Dinheiro
@@ -29,11 +29,11 @@ export const game2Prizes = [
 ];
 
 export const game3Prizes = [
-    {id: "Caixinha de som portátil", winChance: 0.1, imgUrl: "caixinha_som_icon.png", isCashPrize: false}, // Produto físico
-    {id: "Fone Bluetooth básico", winChance: 0.12, imgUrl: "fone_basico_icon.png", isCashPrize: false}, // Produto físico
-    {id: "Smartphone R$1500-2500", winChance: 0.08, imgUrl: "smartphone_icon.png", isCashPrize: false},  // Produto físico
-    {id: "Relógio digital esportivo", winChance: 0.12, imgUrl: "relogio_digital_icon.png", isCashPrize: false}, // Produto físico
-    {id: "PIX R$1000", winChance: 0.1, imgUrl: "pix1000_icon.png", isCashPrize: true},           // Dinheiro
+    {id: "Caixinha de som portátil", winChance: 0.01, imgUrl: "caixinha_som_icon.png", isCashPrize: false}, // Produto físico
+    {id: "Fone Bluetooth básico", winChance: 0.02, imgUrl: "fone_basico_icon.png", isCashPrize: false}, // Produto físico
+    {id: "Smartphone R$1500-2500", winChance: 0.01, imgUrl: "smartphone_icon.png", isCashPrize: false},  // Produto físico
+    {id: "Relógio digital esportivo", winChance: 0.02, imgUrl: "relogio_digital_icon.png", isCashPrize: false}, // Produto físico
+    {id: "PIX R$1000", winChance: 0.01, imgUrl: "pix1000_icon.png", isCashPrize: true},           // Dinheiro
     {id: "PIX R$100", winChance: 0.12, imgUrl: "pix100_icon.png", isCashPrize: true},           // Dinheiro
     {id: "PIX R$50", winChance: 0.12, imgUrl: "pix50_icon.png", isCashPrize: true},             // Dinheiro
     {id: "PIX R$10", winChance: 0.15, imgUrl: "pix10_icon.png", isCashPrize: true},             // Dinheiro
@@ -46,8 +46,8 @@ export const game4Prizes = [
     {id: "Scooter elétrica", winChance: 0.01, imgUrl: "scooter_icon.png", isCashPrize: false},    // Produto físico
     {id: "Xbox Series S/X", winChance: 0.01, imgUrl: "xbox_series_icon.png", isCashPrize: false}, // Produto físico
     {id: "Smartphone R$3000-5000", winChance: 0.01, imgUrl: "smartphone_premium_icon.png", isCashPrize: false}, // Produto físico
-    {id: "Caixa de som inteligente", winChance: 0.01, imgUrl: "caixa_som_inteligente_icon.png", isCashPrize: false}, // Produto físico
-    {id: "Fone Bluetooth", winChance: 0.01, imgUrl: "fone_bluetooth_icon.png", isCashPrize: false}, // Produto físico          // Dinheiro
+    {id: "Caixinha de som portátil", winChance: 0.01, imgUrl: "caixinha_som_icon.png", isCashPrize: false}, // Produto físico
+    {id: "Fone Bluetooth básico", winChance: 0.01, imgUrl: "fone_basico_icon.png", isCashPrize: false}, // Produto físico          // Dinheiro
     {id: "PIX R$100", winChance: 0.01, imgUrl: "pix100_icon.png", isCashPrize: true},            // Dinheiro
     {id: "PIX R$50", winChance: 0.1, imgUrl: "pix50_icon.png", isCashPrize: true},               // Dinheiro
     {id: "PIX R$10", winChance: 0.32, imgUrl: "pix10_icon.png", isCashPrize: true},              // Dinheiro
